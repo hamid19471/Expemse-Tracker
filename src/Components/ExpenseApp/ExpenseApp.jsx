5e474eaea259e06f1073a14f9915e7bab7836b2a
hamid19471 <hamid.19471@gmail.com>
2023-03-04 01:07:08 +0330

    Update: seprate some of part this component to other component like form and add expose compnent for easy managment

--- src/Components/ExpenseApp/ExpenseApp.jsx
+++ src/Components/ExpenseApp/ExpenseApp.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import AddEspense from "../AddExpense/AddEspense";
+import TransAction from "../TeransAction/TransAction";
+
 const ExpenseApp = () => {
     const [income, setIncom] = useState(0);
     const [expense, setExpense] = useState(0);
+    const [transAction, setTransAction] = useState([]);
     return (
         <Container>
-            <IncomeTag>
-                <BalaceText>Balace : {expense - income}</BalaceText>
-                <button>Add</button>
-            </IncomeTag>
-            <ExpenseTag>
-                <ExpanseTagInnerleft>oen</ExpanseTagInnerleft>
-                <div>two</div>
-            </ExpenseTag>
+            <AddEspense income={income} expense={expense} />
+            <TransActionDiv>
+                <TransAction teransAction={transAction} />
+            </TransActionDiv>
         </Container>
     );
 };
@@ -20,37 +20,13 @@ const ExpenseApp = () => {
 export default ExpenseApp;
 
 const Container = styled.div`
-    width: 350px;
+    width: 450px;
     align-items: center;
     justify-content: cente;
     margin: auto;
 `;
 
-const IncomeTag = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-`;
-
-const BalaceText = styled.p`
-    font-size: 1.5rem;
-    text-transform: uppercase;
-`;
-
-const ExpenseTag = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    gap: 10px;
-    width: 100%;
-`;
-
-const ExpanseTagInnerleft = styled.div`
-    width: 175px;
-    height: 75px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: solid #ff4e4e 2px;
-    border-radius: 5px;
+const TransActionDiv = styled(Container)`
+    margin-top: 3rem;
+    font-size: 2rem;
 `;
