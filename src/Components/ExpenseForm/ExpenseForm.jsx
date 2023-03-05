a3c87249a6ad67e23fbac5c45052adaf6f78b81a
hamid19471 <hamid.19471@gmail.com>
2023-03-05 23:58:49 +0330

    Update: add more conditons for add new transaction

--- src/Components/ExpenseForm/ExpenseForm.jsx
+++ src/Components/ExpenseForm/ExpenseForm.jsx
@@ -1,28 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-const ExpenseForm = () => {
+const ExpenseForm = ({ addTransAction }) => {
+    const [formValues, setFormValues] = useState({
+        type: "Expense",
+        amount: 0,
+        description: "",
+    });
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        addTransAction(formValues);
+        setFormValues({
+            type: "Expense",
+            amount: 0,
+            description: "",
+        });
+    };
+
+    const handleFormInputValue = (event) => {
+        setFormValues({
+            ...formValues,
+            [event.target.name]: event.target.value,
+        });
+    };
+
     return (
-        <form>
+        <form onSubmit={handleFormSubmit}>
             <InputDiv>
                 <input
                     className="amount"
-                    type="text"
-                    name="Amount"
+                    type="number"
+                    name="amount"
+                    required
+                    autoFocus
                     placeholder="Enter Amount ..."
+                    onChange={handleFormInputValue}
+                    value={formValues.amount}
                 />
                 <input
                     className="desc"
                     type="text"
-                    name="Description"
+                    name="description"
+                    required
                     placeholder="Enter Description ..."
+                    onChange={handleFormInputValue}
+                    value={formValues.description}
                 />
             </InputDiv>
             <SecondSection>
-                <div>
-                    <input type="radio" name="Income" placeholder="" />
-                    <input type="radio" name="Income" placeholder="" />
-                </div>
-                <button className="formBtn">Save</button>
+                <RadioBtnDiv>
+                    <div className="income">
+                        <input
+                            type="radio"
+                            name="type"
+                            value="Income"
+                            onChange={handleFormInputValue}
+                            checked={formValues.type === "Income"}
+                        />
+                        <label>Income</label>
+                    </div>
+                    <div className="expense">
+                        <input
+                            type="radio"
+                            name="type"
+                            value="Expense"
+                            onChange={handleFormInputValue}
+                            checked={formValues.type === "Expense"}
+                        />
+                        <label>Expense</label>
+                    </div>
+                </RadioBtnDiv>
+                <button className="formBtn" type="submit">
+                    Save
+                </button>
             </SecondSection>
         </form>
     );
@@ -48,4 +98,23 @@ const SecondSection = styled.div`
     display: flex;
     align-items: center;
     justify-content: space-between;
+`;
+
+const RadioBtnDiv = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    font-size: 1.2rem;
+    .expense {
+        color: #ff4e4e;
+    }
+    .income {
+        color: #4eff98;
+    }
+    text-transform: uppercase;
+    input[type="radio"] {
+        transform: scale(1.4);
+        margin: 0 0.5rem;
+    }
 `;
